ca88b61cc00532423fc1a96e5963316446108e12
jemima92 <161817635+jemima92@users.noreply.github.com>
2024-09-24 15:37:52 +0200

    hi

--- tpb2/main.go
+++ tpb2/main.go
@@ -1,10 +1,6 @@
 package main
 
-import (
-	"b2"
-	"fmt"
-)
-
+/*
 func main() {
 	fmt.Println(b2.Ft_coin([]int{1, 2, 5}, 11))
 	fmt.Println(b2.Ft_coin([]int{2}, 3))
@@ -16,3 +12,21 @@ func main() {
 	fmt.Println(exo2.Ft_missing([]int{0, 1}))
 	fmt.Println(exo2.Ft_missing([]int{9, 6, 4, 2, 3, 5, 7, 0, 1}))
 }*/
+/*
+func main() {
+
+	fmt.println(exo3.Ft_non_overlap([][]int{{1, 2}, {2, 3}, {3, 4}, {1, 3}}))
+	fmt.println(exo3.Ft_non_overlap([][]int{{1, 2}, {2, 3}}))
+	fmt.println(exo3.Ft_non_overlap([][]int{{1, 2}, {1, 2}, {1, 2}}))
+}
+
+*/
+/*
+func main() {
+	println(exo.Ft_profit([]int{7, 1, 5, 3, 6, 4})) // resultat : 5
+	// si on achète au jour 1, nous payons 1,
+	// et si nous le vendons au 4eme jour, nous gagnons 6, le bénéfice est 6-1
+	println(exo.Ft_profit([]int{7, 6, 4, 3, 1})) // resultat : 0
+}
+
+*/
