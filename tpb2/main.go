a2070f91f5c5ba6640390c82eb7f747beb964e8f
jemima92 <161817635+jemima92@users.noreply.github.com>
2024-09-24 15:14:38 +0200

    nk

---
+++ tpb2/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"b2"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(b2.Ft_coin([]int{1, 2, 5}, 11))
+	fmt.Println(b2.Ft_coin([]int{2}, 3))
+}
+
+/*
+func main() {
+	fmt.Println(exo2.Ft_missing([]int{3, 1, 2}))
+	fmt.Println(exo2.Ft_missing([]int{0, 1}))
+	fmt.Println(exo2.Ft_missing([]int{9, 6, 4, 2, 3, 5, 7, 0, 1}))
+}*/
